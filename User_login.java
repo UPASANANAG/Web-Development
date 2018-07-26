01872c7b2714beb7535d469126f1ffe9734cd56c
Upasana Nag <35536094+UPASANANAG@users.noreply.github.com>
2018-07-27 00:40:35 +0530

    Added JAVA class to initialize variables

---
+++ User_login.java
@@ -0,0 +1,37 @@
+package HRManagement;
+
+public class User_login 
+{
+	private String name;
+	private int id;
+	private String password;
+	private String desn;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDesn() {
+		return desn;
+	}
+	public void setDesn(String desn) {
+		this.desn = desn;
+	}
+	
+	
+}
