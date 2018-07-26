6a8c63f4b497909ef9861d245e40a35c3b7e576c
Upasana Nag <35536094+UPASANANAG@users.noreply.github.com>
2018-07-27 00:39:42 +0530

    Added Java Class to initialize variables

---
+++ Resource_generate.java
@@ -0,0 +1,50 @@
+package HRManagement;
+
+public class Resource_generate {
+	private String Name;
+	private int rec_ID;
+	private String skills;
+	private String experience;
+	private String Status;
+	private String Date;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getRec_ID() {
+		return rec_ID;
+	}
+	public void setRec_ID(int rec_ID) {
+		this.rec_ID = rec_ID;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	
+
+}
